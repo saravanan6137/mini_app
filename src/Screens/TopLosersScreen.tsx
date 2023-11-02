93de1e5c926c0b9ad836c69ca1e4346ebde1e890
Saravanan-Kannan <saravanan.kannan@maybank.com>
2023-11-02 12:23:32 +0800

    fixes

--- src/Screens/TopLosersScreen.tsx
+++ src/Screens/TopLosersScreen.tsx
@@ -1,82 +1,38 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, Platform} from 'react-native';
-import {RNCamera} from 'react-native-camera';
-import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import React, { useState, useEffect } from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const TopLosersScreen: React.FC = () => {
+
+
+const TopLosersScreen = () => {
   const [scanned, setScanned] = useState(false);
-  const [scannedData, setScannedData] = useState<string | null>(null);
-  const [cameraPermission, setCameraPermission] = useState<boolean | null>(
-    null,
-  );
+  const [scannedData, setScannedData] = useState('');
 
-  useEffect(() => {
-    async function requestCameraPermission() {
-      const permission = Platform.select({
-        ios: PERMISSIONS.IOS.CAMERA,
-        android: PERMISSIONS.ANDROID.CAMERA,
-      });
-      if (!permission) {
-        // Handle unsupported platform or other issues
-        return;
-      }
-      try {
-        const status = await check(permission);
-
-        if (status === RESULTS.GRANTED) {
-          setCameraPermission(true);
-        } else {
-          const requestStatus = await request(permission);
-          if (requestStatus === RESULTS.GRANTED) {
-            setCameraPermission(true);
-          } else {
-            setCameraPermission(false);
-          }
-        }
-      } catch (error) {
-        console.warn(error);
-        setCameraPermission(false);
-      }
-    }
-
-    requestCameraPermission();
-  }, []);
-
-  const handleBarCodeScanned = ({data}: {data: string}) => {
+  const handleBarCodeScanned = ({ data }: { data: string }) => {
     if (!scanned) {
       setScanned(true);
       setScannedData(data);
     }
   };
 
-  if (cameraPermission === null) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Requesting Camera Permission...</Text>
-      </View>
-    );
-  } else if (cameraPermission === false) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>
-          Camera permission denied. Please enable it in settings.
-        </Text>
-      </View>
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        <RNCamera
-          style={styles.camera}
-          onBarCodeRead={handleBarCodeScanned}
-          captureAudio={false}
-        />
-        <Text style={styles.text}>
-          {scannedData ? `Scanned Data: ${scannedData}` : 'Scan a QR code'}
-        </Text>
-      </View>
-    );
-  }
+  useEffect(() => {
+    setScanned(false);
+    setScannedData('');
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <RNCamera
+        style={styles.camera}
+        onBarCodeRead={handleBarCodeScanned}
+        captureAudio={false}
+      />
+      <Text style={styles.text}>
+        {scanned ? `Scanned Data: ${scannedData}` : 'Scan a QR code'}
+      </Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
